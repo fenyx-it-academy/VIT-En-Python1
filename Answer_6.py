f7f054eda23cb0fa0f7b22b264fda0dcccb63511
Dastgirzada <nasirdastgirzada@gmail.com>
2023-11-08 10:36:32 +0100

    My Home work answers
    I have done but not all of them.

---
+++ Answer_6.py
@@ -0,0 +1,20 @@
+print("Hello! Let's have a conversation.")
+
+name = input("What's your name? ")
+print(f"Nice to meet you, {name}!")
+
+age = input("How old are you? ")
+age = int(age) 
+print("Great")
+color = input("What's your favorite color? ")
+print(f"Ah, {color} is a lovely color!")
+
+location = input("Where are you from? ")
+print(f"I've heard {location} is a beautiful place!")
+
+hobby = input("What are you have any hobbies? ")
+print("Very nice")
+
+questions = input("What is your dream job? ")
+print("Nice job!")
+print("It was great chatting with you. Have a wonderful day!")
