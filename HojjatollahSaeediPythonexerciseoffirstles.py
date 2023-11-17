593d4e195cce413e6ea484e109ce63a77229e5d0
Hjtos <150122904+Hjtos@users.noreply.github.com>
2023-11-17 15:09:41 +0100

    Update HojjatollahSaeediPythonexerciseoffirstles.py

--- HojjatollahSaeediPythonexerciseoffirstles.py
+++ HojjatollahSaeediPythonexerciseoffirstles.py
@@ -68,7 +68,86 @@ print(seconds, "Seconds is\n", days, "Days\n", hours, "Hours\n", minutes, "Minut
 
 #End of fourth exercise
 
+# Fifth exercise
+# Print name  in a rectangle
+
+fname = input("Enter your first name, please: ")
+lname = input("Enter your last name, please: ")
+
+fnum = len(fname)
+lnum = len(lname)
+star = '**'
+
+x = fnum + lnum
+i = 0
+
+while i <= x :
+    star = star + '*'
+    i += 1
 
 
+i = 1
+space = ''
+while i <= len(star) :
+    space = space + ' '
+    i += 1
+
+i = 0
+outputstr = ''
+while i <= 4 :
+    if i == 0 or i == 4 :
+        outputstr += '\nI' + star + 'I'
+    elif i == 2 :
+        outputstr += '\nI ' + fname + ' ' + lname + ' I'
+    else:  outputstr += '\nI' + space + 'I'
+    i += 1
+
+print(outputstr)
+
+#End of fifth exercise
+
+# Sixth exercise
+# A short chat
+
+print("This is the sixth exercise that have a short conversation with you")
+print(" Come to have a short conversation together")
+
+yourname = input("What is your name:")
+print("Hello ", yourname)
+
+yourage = int(input("Please, enter your age: "))
+
+if yourage < 13 :
+    print("You are very young")
+elif yourage >= 13 or yourage <= 19 :
+    print("You are a teenager")
+elif yourage >= 20 or yourage <= 39 :
+    print("You are a young person")
+elif yourage >= 40 or yourage <= 55 :
+    print("You are a middelage person")
+elif yourage >= 56 :
+    print("You are a old person")
+
+yourcountry = input("Where are you from ?")
+yourplace = input("Wich country do you live now ? ")
+
+if yourcountry == yourplace :
+    print("So you live fortunately in your country")
+else :
+    print("Unfortunately you are a emigrant")
+
+marriagestatus = bool(input("Are you married ? (if NO, please enter 0, other Numbers is yes)!"))
+
+if marriagestatus == True :
+     childnum = int(input("How many childeren do you have ? "))
+     if childnum <= 2 :
+         print("You have a small family")
+     else :
+         print("you have a big family")
+else :
+    print("I hope that you find a good partner")
 
 
+#End of sixth exercise
+
+#End of the all of exercises
